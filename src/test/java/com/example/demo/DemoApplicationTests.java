c42bda8c47aa8dd74ae9c8e041adc4524c450f11
wikawikunia11 <usiamal@gmail.com>
2026-03-14 11:58:37 +0100

    added sanity check test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void sanityCheck(){
+		assertEquals(4, 2+2);
 	}
 
 }
